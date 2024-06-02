91f0aa94051af5c401f2e9f83237ee4377a14146
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2024-06-02 19:37:09 +0700

    ^update question repository

--- internal/repo/question.go
+++ internal/repo/question.go
@@ -2,24 +2,26 @@ package repo
 
 import (
 	"database/sql"
+	"time"
 
 	"github.com/fajaralfa/askme/internal/model"
 )
 
 type QuestionInterface interface {
-	FindAssociatedWithUser(email string) ([]model.Question, error)
-	Find(id string) (*model.Question, error)
-	Create(text string, userId string) (*model.Question, error)
-	Delete(id string) error
+	FindAllByUserEmail(userid string) ([]model.Question, error)
+	FindAllByUserId(userid string) ([]model.Question, error)
+	FindByUserId(qid, userid string) (*model.Question, error)
+	Create(questionText string, userid int64) (*model.Question, error)
+	DeleteByQIdUserId(id, userid string) (int64, error)
 }
 
 type Question struct {
 	Db *sql.DB
 }
 
-func (q *Question) FindAssociatedWithUser(email string) ([]model.Question, error) {
+func (q *Question) FindAllByUserEmail(email string) ([]model.Question, error) {
 	questions := make([]model.Question, 0)
-	rows, err := q.Db.Query("SELECT questions.id, questions.question, questions.created_at, questions.user_id FROM questions LEFT JOIN users ON users.id = questions.user_id  WHERE users.email = ?", email)
+	rows, err := q.Db.Query("SELECT questions.* FROM questions LEFT JOIN users ON user_id = users.id WHERE email = ?", email)
 	if err != nil {
 		return nil, err
 	}
@@ -27,7 +29,7 @@ func (q *Question) FindAssociatedWithUser(email string) ([]model.Question, error
 
 	for rows.Next() {
 		var question model.Question
-		if err := rows.Scan(&question.Id, &question.Text, &question.Date, &question.UserId); err != nil {
+		if err := rows.Scan(&question.Id, &question.Question, &question.CreatedAt, &question.UserId); err != nil {
 			return nil, err
 		}
 		questions = append(questions, question)
@@ -39,16 +41,64 @@ func (q *Question) FindAssociatedWithUser(email string) ([]model.Question, error
 	return questions, nil
 }
 
-func (q *Question) Find(id string) (*model.Question, error) {
-	question := new(model.Question)
+func (q *Question) FindAllByUserId(userid string) ([]model.Question, error) {
+	questions := make([]model.Question, 0)
+	rows, err := q.Db.Query("SELECT * FROM questions WHERE user_id = ?", userid)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var question model.Question
+		if err := rows.Scan(&question.Id, &question.Question, &question.CreatedAt, &question.UserId); err != nil {
+			return nil, err
+		}
+		questions = append(questions, question)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return questions, nil
+}
+
+func (q *Question) FindByUserId(qid, userid string) (*model.Question, error) {
+	var question *model.Question
+	rows, err := q.Db.Query("SELECT * FROM questions WHERE id = ? AND user_id = ?", qid, userid)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		question = new(model.Question)
+		if err := rows.Scan(&question.Id, &question.Question, &question.CreatedAt, &question.UserId); err != nil {
+			return nil, err
+		}
+	}
+
 	return question, nil
 }
 
-func (q *Question) Create(text string, userId string) (*model.Question, error) {
+func (q *Question) Create(questionText string, userid int64) (*model.Question, error) {
 	question := new(model.Question)
+	created_at := time.Now()
+	res, err := q.Db.Exec("INSERT INTO questions (question, created_at, user_id) VALUES (?,?,?)", questionText, time.Now(), userid)
+	if err != nil {
+		return nil, err
+	}
+	lastid, _ := res.LastInsertId()
+
+	question.Id = lastid
+	question.Question = questionText
+	question.CreatedAt = created_at.String()
+	question.UserId = userid
 	return question, nil
 }
 
-func (q *Question) Delete(id string) error {
-	return nil
+func (q *Question) DeleteByQIdUserId(id, userid string) (int64, error) {
+	res, err := q.Db.Exec("DELETE FROM questions WHERE id = ? and user_id = ?", id, userid)
+	affected, _ := res.RowsAffected()
+	return affected, err
 }
