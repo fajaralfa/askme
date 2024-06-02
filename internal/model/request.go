4dd6b8a20dca1f3a16c79953f791f0f4c6ca3fbf
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2024-06-02 19:37:26 +0700

    + AskQuestion request

--- internal/model/request.go
+++ internal/model/request.go
@@ -9,3 +9,8 @@ type LoginReq struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
+
+type AskQuestionReq struct {
+	Question    string `json:"question"`
+	TargetEmail string `json:"targetEmail"`
+}
