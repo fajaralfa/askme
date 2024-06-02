409be1c4adffe03c80079a8702940636ffc26633
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2024-06-02 10:02:00 +0700

    + authenticated middleware

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -2,11 +2,33 @@ package middleware
 
 import (
 	"net/http"
+
+	"github.com/fajaralfa/askme/internal/handler"
+	"github.com/fajaralfa/askme/internal/service/jwt"
 )
 
 func Api(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("content-type", "application/json")
 		next.ServeHTTP(w, r)
+	})
+}
+
+func Authenticated(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenString := r.Header.Get("Authorization")
+		if tokenString == "" {
+			handler.ApiUnauthorizedErr(w, "missing authorization header", nil)
+			return
+		}
+		tokenString = tokenString[len("Bearer "):]
+		token, _ := jwt.Parse(tokenString)
+
+		if token.Valid {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		handler.ApiUnauthorizedErr(w, "auth token invalid", nil)
 	})
 }
