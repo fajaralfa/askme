ca98e7950ef13dfe09e5344e4aa7b45ed68e95d0
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2024-06-10 02:18:25 +0700

    + loading spinner & suspense

--- web/App.vue
+++ web/App.vue
@@ -1,7 +1,15 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import Spinner from './icon/Spinner.vue';
+import { isLoading } from './store/loadingPageStore';
+import { Suspense } from 'vue';
+
+const loading = isLoading()
 </script>
 
 <template>
-    <RouterView />
+    <Spinner class="animate-spin w-10 h-10 right-2 top-2 fixed" v-show="loading" />
+    <Suspense>
+        <RouterView />
+    </Suspense>
 </template>
