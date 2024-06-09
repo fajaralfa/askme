18ebc6cff9616b06a3b5e38ec3dc8da893edcbd8
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2024-06-09 22:53:21 +0700

    place content to center (home & register)

--- web/view/HomeView.vue
+++ web/view/HomeView.vue
@@ -1,6 +1,4 @@
 <script setup>
-import { Teleport, Transition, ref } from 'vue';
-import Copy from '@/icon/Copy.vue'
 import Nav from '@/component/Nav.vue';
 import useUserStore from '@/composable/useUserStore';
 import LogoutButton from '@/component/LogoutButton.vue'
@@ -9,33 +7,16 @@ const user = useUserStore()
 
 const link = `${window.location.origin}/${user.get().value.email}`
 
-async function copyToClipboard(str) {
-    await navigator.clipboard.writeText(str)
-    toggle.value = true
-}
-
-const toggle = ref(false)
-
 </script>
 
 <template>
     <Nav></Nav>
     <LogoutButton />
-    <div class="text-base mt-5 p-3 border rounded inline box-border active:outline-white active:outline active:outline-1">
-        <span>{{ link }}</span> <Copy @click="copyToClipboard(link)" width="1.5rem" height="1.5rem" style="display: inline;" /></div>
-    <Teleport to="body">
-        <Transition
-        enter-from-class="opacity-0"
-        enter-active-class="transition-opacity duration-150 ease"
-        leave-active-class="transition-opacity duration-150 ease"
-        leave-from-class="opacity-0"
-        leave-to-class="opacity-0"
-        class=""
-        >
-            <div v-if="toggle" @click="toggle = false"
-                class="fixed top-0 bg-[rgba(0,0,0,0.3)] w-screen h-screen flex justify-center items-center transition">
-                <div class="bg-white text-black px-4 py-2 rounded">link disalin!</div>
-            </div>
-        </Transition>
-    </Teleport>
+    <div class="max-w-2xl m-auto">
+        <div class="flex flex-col items-center gap-10 mt-28">
+            <h1>Salin Link Dibawah</h1>
+            <div class="text-base p-3 border rounded box-border active:outline-white active:outline active:outline-1"
+            >{{ link }}</div>
+        </div>
+    </div>
 </template>
--- web/view/RegisterView.vue
+++ web/view/RegisterView.vue
@@ -6,16 +6,16 @@ const {form, loading, errMessage, submit} = useRegister()
 </script>
 
 <template>
-    <div class="p-3">
+    <div class="w-[100vw] h-[100vh] grid place-items-center fixed">
         <form>
             <div class="bg-red-400 text-white px-3 py-1" v-if="errMessage">{{ errMessage }}</div>
             <div class="space-y-2">
-                <input v-model="form.email" type="email" name="email" id="email" placeholder="example@email.com"
-                    class="block rounded">
+                <input v-model="form.email" type="text" name="username" id="username" placeholder="username"
+                    class="block rounded p-1 border">
                 <input v-model="form.password" type="password" name="password" id="password" placeholder="*******"
-                    class="block rounded">
-                <button @click="submit" type="submit" class="bg-white px-4 py-2 rounded" :disabled="loading">
-                    <span v-if="loading">Loading...</span><span v-else>Register</span>
+                    class="block rounded p-1 border">
+                <button @click="submit" type="submit" class="px-4 py-2 rounded-xl bg-blue-700 text-white font-bold" :disabled="loading">
+                    <span v-if="loading">Loading...</span><span v-else>REGISTER</span>
                 </button>
             </div>
         </form>
