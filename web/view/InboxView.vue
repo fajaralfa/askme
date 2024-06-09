48912282574451397d92cf40a7b45979c12f68bc
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2024-06-10 02:20:34 +0700

    out of word, update inbox view & set seen status

--- web/view/InboxView.vue
+++ web/view/InboxView.vue
@@ -3,16 +3,22 @@ import useGetQuestions from '@/composable/useGetQuestions';
 import Nav from '@/component/Nav.vue';
 import QuestionModal from '@/component/QuestionModal.vue'
 import LogoutButton from '@/component/LogoutButton.vue'
+import InboxNew from '@/icon/InboxNew.vue';
+import Inbox from '@/icon/Inbox.vue';
 import { ref } from 'vue';
+import { fetchJSONWithAuth } from '@/helper/fetch';
+import * as user from '@/store/userStore'
 
 const {questions, fetchQuestions, fetchStatus} = useGetQuestions()
 
-fetchQuestions()
+await fetchQuestions()
 
 const question = ref('')
 const show = ref(false)
-function showModal(q) {
-    question.value = q
+async function showQuestion(item) {
+    await fetchJSONWithAuth(`/api/v1/questions/${item.id}`, user.get().value.accessToken) // trigger seen status
+    question.value = item.question
+    item.seen = true
     show.value = true
 }
 
@@ -22,10 +28,14 @@ function showModal(q) {
     <Nav />
     <LogoutButton />
     <div class="max-w-2xl m-auto">
-        <div class="bg-red-500" v-if="questions.length == 0">data kosong</div>
+        <div class="flex flex-col items-center gap-10 mt-28">
+            <div class="" v-if="questions.length == 0">data kosong!</div>
+        </div>
         <div v-if="fetchStatus === 'success'" class="grid grid-cols-3 gap-5 place-items-center p-10">
-            <button v-for="item in questions" @click="showModal(item.question)"
-                class="bg-blue-500 h-24 w-24 rounded-3xl"></button>
+            <button v-for="item in questions" @click="showQuestion(item)" class="bg-blue-300 rounded-3xl">
+                <Inbox v-if="item.seen" class="h-24 w-24" />
+                <InboxNew v-else class="h-24 w-24" />
+            </button>
         </div>
     </div>
     <QuestionModal title="Ask Me Anything!" :question :show @show="(e) => show=e" />
