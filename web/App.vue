124674a7f7b76378de411098bb88315b733ff035
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2024-06-10 03:09:56 +0700

    + fallback template (loading spinner)

--- web/App.vue
+++ web/App.vue
@@ -11,5 +11,10 @@ const loading = isLoading()
     <Spinner class="animate-spin w-10 h-10 right-2 top-2 fixed" v-show="loading" />
     <Suspense>
         <RouterView />
+        <template #fallback>
+            <div class="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-1">
+                <Spinner class="animate-spin w-52 h-52" />
+            </div>
+        </template>
     </Suspense>
 </template>
