5a47e67b0d43d0e91282168fd37dcf5634e3cbb4
fajaralfa <fajaralfrzi@gmail.com>
2025-07-27 19:50:47 +0700

    update deps, remove unnecessary config

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,36 @@
-import { defineConfig, loadEnv } from 'vite'
-import vuePlugin from '@vitejs/plugin-vue'
-import path from 'node:path'
+import { defineConfig, loadEnv } from "vite";
+import vuePlugin from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 
 export default defineConfig(({ command, mode }) => {
-    const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), "");
 
-    return {
-        plugins: [vuePlugin()],
-        server: {
-            host: env.VITE_HOST,
-            port: env.VITE_PORT,
+  return {
+    plugins: [
+      vuePlugin(),
+      tailwindcss({
+        content: ["./web/index.html", "./web/**/*.vue"],
+        theme: {
+          extend: {},
         },
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, './web'),
-            },
-        },
-        build: {
-            manifest: true,
-            rollupOptions: {
-                input: '/web/main.js',
-            },
-        }
-    }
-})
+        plugins: [],
+      }),
+    ],
+    server: {
+      host: env.VITE_HOST,
+      port: env.VITE_PORT,
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./web"),
+      },
+    },
+    build: {
+      manifest: true,
+      rollupOptions: {
+        input: "/web/main.js",
+      },
+    },
+  };
+});
